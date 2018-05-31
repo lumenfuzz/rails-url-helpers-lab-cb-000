cf7025b62c4986d579ee86fc45d26a7c1e26ef58
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-31 22:56:10 +0000

    automatically backed up by learn

--- db/migrate/002_add_active_to_students.rb
+++
@@ -1,9 +0,0 @@
-class AddActiveToStudents < ActiveRecord::Migration
-  def up
-    add_column :students, :active, :boolean, default: false
-  end
-
-  def down
-    remove_column :students, :active
-  end
-end

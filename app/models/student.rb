072950746c69d1cfde72541a8e41de1ee2ff7b1a
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-31 13:50:56 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,10 +4,6 @@ class Student < ActiveRecord::Base
     super
     @active = false
   end
-  def self.create!(params)
-    super
-    @active = false
-  end
   def to_s
     self.first_name + " " + self.last_name
   end

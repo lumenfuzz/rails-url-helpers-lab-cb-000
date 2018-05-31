c88e20c31266af406101f8df7dcb0be2257ed6f3
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-31 22:02:05 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,9 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+    @students.each do |student|
+      @student.active = true
+    end
   end
 
   def show

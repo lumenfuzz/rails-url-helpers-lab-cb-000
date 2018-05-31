810bb558655d3b984a90be92744eeec02a92bac8
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-31 22:39:07 +0000

    automatically backed up by learn

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,7 +1,7 @@
 class AddActiveToStudents < ActiveRecord::Migration
   def change
     change_table :students do |t|
-      t.boolean :active
+      t.boolean :active, default: false
     end
   end
 end

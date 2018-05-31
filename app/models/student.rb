5efc7dda08402e6f8637eac0690cc09682d8a803
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-31 22:13:20 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,12 +1,9 @@
 class Student < ActiveRecord::Base
   attr_accessor :active
   after_initialize :set_defaults, unless: :persisted?
+  after_create! :set_defaults, unless: :persisted?
   def to_s
     self.first_name + " " + self.last_name
-  end
-  def self.create!(params)
-    super
-    self.active = false if self.active.nil?
   end
   def set_defaults
     self.active = false if self.active.nil?

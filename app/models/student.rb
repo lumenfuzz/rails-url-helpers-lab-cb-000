6e04b600985f1defd335e53c2bf5bf8f4411f6ce
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-31 13:57:39 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,10 @@
 class Student < ActiveRecord::Base
   attr_accessor :active
-  def initialize(params)
-    super
-    @active = false
-  end
+  after_initalize :init
   def to_s
     self.first_name + " " + self.last_name
+  end
+  def init
+    self.active = false if self.active.nil?
   end
 end

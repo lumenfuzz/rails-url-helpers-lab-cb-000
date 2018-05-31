f4fd42cc81b221e5251b924555aa1dc7ecc4f53d
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-31 21:01:22 +0000

    automatically backed up by learn

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -58,6 +58,7 @@ describe 'Activate page' do
 
   it "Should mark an inactive student as active" do
     visit activate_student_path(@student)
+    byebug
     @student.reload
     expect(@student.active).to eq(true)
   end
